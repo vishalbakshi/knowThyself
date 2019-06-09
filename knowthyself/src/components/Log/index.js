4a809636eac85aeb00c6fdb77504b5b9d9230390
Vishal Bakshi <vdbakshi@gmail.com>
2019-06-08 23:24:45 -0700

    initial tutorial version

---
+++ knowthyself/src/components/Log/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Log = () => (
+    <div>
+        <h1>Log</h1>
+    </div>
+);
+
+export default Log;
---
+++ knowthyself/src/components/ThoughtTypes/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ThoughtTypes = () => (
+    <div>
+        <h1>Thought Types</h1>
+    </div>
+);
+
+export default ThoughtTypes;
