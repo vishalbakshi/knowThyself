91fe497570ec2eb370bfe16a25cc1ef229d35191
Vishal Bakshi <vdbakshi@gmail.com>
2019-06-09 21:58:07 -0700

    finish course content

--- knowthyself/src/components/Landing/index.js
+++ knowthyself/src/components/Landing/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Landing = () => (
     <div>
-        <h1>Landing</h1>
+        <h1>knowThyself</h1>
     </div>
 );
 
---
+++ knowthyself/src/components/Session/withAuthorization.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import AuthUserContext from './context';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+/**
+ * Renders protected page if user is authorized
+ * Redirects to sign in page is user is not authorized
+ * 
+ * @param {function} condition - user authorization condition
+ */
+const withAuthorization = condition => Component => {
+    class WithAuthorization extends React.Component {
+        // redirects to sign in page if user does not meet
+        // authorization condition
+
+        componentDidMount() {
+            this.listener = this.props.firebase.auth.onAuthStateChanged(
+                authUser => {
+                    if (!condition(authUser)) {
+                        this.props.history.push(ROUTES.SIGN_IN);
+                    }
+                },
+            );
+        }
+
+        componentWillUnmount() {
+            this.listener();
+        }
+
+        render() {
+            return (
+                <AuthUserContext.Consumer>
+                    {authUser =>
+                        condition(authUser) ? <Component {...this.props} /> : null
+                    }
+                </AuthUserContext.Consumer>
+            );
+        }
+    }
+
+    return compose(
+        withRouter,
+        withFirebase,
+    )(WithAuthorization);
+};
+
+export default withAuthorization;
